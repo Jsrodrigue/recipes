39fab25b09e493896e18b55ae82e5de9b8101a4a
Jsrodrigue <jsrodriguez16@gmail.com>
2025-07-02 08:41:42 -0500

    add js to promt ingredients and include them in the form

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, HiddenField, SubmitField, SelectMultipleField, FileField
-from wtforms.validators import DataRequired, FileAllowed
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed
 
 # Form to create a new recipe
 class NewRecipeForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description')
     instructions = TextAreaField('Instructions')
-    # Use a hiddenField because we will save the ingredients in a list of dictionaries with keyw 'name' 'qunantity'
+    # Use a hiddenField because we will save the ingredients in a list of dictionaries with keys 'name' 'quantity'
     ingredients = HiddenField('Ingredients (JSON)', validators=[DataRequired()])
     tags = SelectMultipleField('Tags', coerce=int)
     photo = FileField('Recipe Photo', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
--- recipes/routes.py
+++ recipes/routes.py
@@ -1,8 +1,18 @@
 from flask import Blueprint, render_template, redirect, flash, url_for
 from flask_login import login_required, current_user
+from .forms import NewRecipeForm
+from models import Recipe
 
 recipes = Blueprint('recipes', __name__)
 
 @recipes.route('/')
 def index():
   return render_template('recipes/recipes_manage.html', user=current_user)
+
+@recipes.route('/new', methods=["GET", "POST"])
+@login_required
+def new():
+  form = NewRecipeForm()
+  if form.validate_on_submit:
+    recipe = Recipe(title=form.title.data, description=form.description.data, )
+  return render_template("recipes/add_recipe_form.html", form=form)
